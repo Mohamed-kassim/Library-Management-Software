edca29983798cc972c9ba2855eda4f80077fabdb
MohamedWagih <mohamed0wagih@gmail.com>
2018-05-13 21:41:33 +0200

    changes in UI

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ mongoose.connect('mongodb://localhost/Library');
 mongoose.Promise = global.Promise;
 
 // middlewares
+app.use(express.static('assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -16,6 +17,7 @@ app.use('/books',require('./api/controllers/booksController') );
 app.use('/members',require('./api/controllers/membersController') );
 app.use('/users',require('./api/controllers/usersController') );
 app.use('/managers',require('./api/controllers/managersController') );
+
 
 
 // start the server
